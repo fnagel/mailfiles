738b9080e74551f7d311ec9a9acf6b92a220cc9a
Felix Nagel <info@felixnagel.com>
2015-03-18 16:54:31 +0100

    Clean up ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,30 +16,22 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'plugin',
 	'author' => 'Felix Nagel',
 	'author_email' => 'info@felixnagel.com',
-	'shy' => '',
-	'dependencies' => 'pluploadfe',
-	'conflicts' => 'form',
-	'priority' => '',
-	'module' => '',
 	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => '',
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.1.0-6.2.99',
-			'pluploadfe' => '1.0.0-',
+			'cms' => '',
+			'pluploadfe' => '1.0.0-1.1.99',
 		),
-		'conflicts' => array(),
+		'conflicts' => array(
+			'form' => '',
+		),
 		'suggests' => array(),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"382f";s:12:"ext_icon.gif";s:4:"77b3";s:17:"ext_localconf.php";s:4:"3531";s:14:"ext_tables.php";s:4:"438e";s:14:"ext_tables.sql";s:4:"872b";s:13:"locallang.xml";s:4:"423c";s:16:"locallang_db.xml";s:4:"201a";s:10:"README.txt";s:4:"0c4a";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_mailfiles_pi1.php";s:4:"5290";s:38:"pi1/class.tx_mailfiles_pi1_wizicon.php";s:4:"e2ee";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"4d04";s:20:"pi1/static/setup.txt";s:4:"e054";s:17:"res/template.html";s:4:"8779";}',
-	'suggests' => array(),
 );
 
 ?>
