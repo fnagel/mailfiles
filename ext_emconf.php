c578f4de4dc4c1cfce8bf0ca291385b3ab6e618e
Felix Nagel <info@felixnagel.com>
2016-08-14 23:38:22 +0200

    [TASK] Consider extension TYPO3 8.2 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4.0-7.0.99',
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '6.2.0-8.2.99',
 			'cms' => '',
 			'pluploadfe' => '1.2.0-1.4.99',
 		),
