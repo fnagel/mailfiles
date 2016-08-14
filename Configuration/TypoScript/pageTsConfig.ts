a8d112bcc83a53d7089691e4e2ad2540dcb18208
Felix Nagel <info@felixnagel.com>
2016-08-14 23:42:41 +0200

    [FIX] Fix plugin wizard icon for TYPO3 6.2

--- Configuration/TypoScript/pageTsConfig.ts
+++ Configuration/TypoScript/pageTsConfig.ts
@@ -1,6 +1,8 @@
 mod.wizards.newContentElement.wizardItems.plugins {
 	elements.mailfiles {
 		iconIdentifier = extensions-mailfiles-wizard
+		# Fallback for TYPO3 6.2
+		icon = ../typo3conf/ext/mailfiles/pi1/ce_wiz.gif
 		title = LLL:EXT:mailfiles/locallang.xml:pi1_title
 		description = LLL:EXT:mailfiles/locallang.xml:pi1_plus_wiz_description
 		tt_content_defValues.CType = list
--- ext_tables.php
+++ ext_tables.php
@@ -19,16 +19,8 @@ if (!defined('TYPO3_MODE')) {
 	$_EXTKEY . '_pi1'
 ), 'list_type');
 
-
-// Use old icon path for TYPO3 6.2
 // @todo Remove this when 6.2 is no longer relevant
-if (version_compare(TYPO3_branch, '7.0', '<')) {
-    $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
-
-    \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons(array(
-        'wizard' => $extensionPath . 'pi1/ce_wiz.gif',
-    ), 'mailfiles');
-} else {
+if (version_compare(TYPO3_branch, '7.0', '>=')) {
     /* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     $iconRegistry->registerIcon(
