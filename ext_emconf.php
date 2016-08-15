f1139b10ce6f90a0f0ad90783687ad27ae494ca3
Felix Nagel <info@felixnagel.com>
2016-08-15 11:06:38 +0200

    Maintenance release: TYPO3 8.2 support
    Changelog: https://github.com/fnagel/mailfiles/compare/1.1.3...1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,13 +20,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'internal' => '',
 	'uploadfolder' => 0,
 	'clearCacheOnLoad' => 0,
-	'version' => '1.1.4-dev',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4.0-7.0.99',
 			'typo3' => '6.2.0-8.2.99',
 			'cms' => '',
-			'pluploadfe' => '1.2.0-1.4.99',
+			'pluploadfe' => '1.2.0-1.5.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
