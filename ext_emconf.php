e8f638d3e94a8f6ca74bed92b190848a17843d8c
Felix Nagel <info@felixnagel.com>
2018-08-09 10:15:12 +0200

    [TASK] Update EXT:pluploadfe constraints for upcoming release

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'php' => '5.5.0-7.1.99',
             'typo3' => '7.6.0-8.7.99',
-            'pluploadfe' => '1.4.0-1.6.99',
+            'pluploadfe' => '1.4.0-2.0.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
