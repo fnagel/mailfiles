9ffe7b2e3f00319289847da7f18c9220f089ba4b
Felix Nagel <info@felixnagel.com>
2020-10-16 15:25:11 +0200

    [TASK] Consider extension PHP 7.4 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.3-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.2.99',
+            'php' => '7.0.0-7.4.99',
             'typo3' => '9.0.0-10.4.99',
             'pluploadfe' => '2.0.0-4.9.99',
         ],
