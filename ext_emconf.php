f48480155d4412f356fa0904545b082481da4065
Felix Nagel <info@felixnagel.com>
2025-12-18 11:10:05 +0100

    [!!!][TASK] Adjust TypoScript to EXT:pluploadfe content types migration

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '8.2.0-8.4.99',
             'typo3' => '13.3.0-13.4.99',
-            'pluploadfe' => '7.0.0-8.9.99',
+            'pluploadfe' => '9.0.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
